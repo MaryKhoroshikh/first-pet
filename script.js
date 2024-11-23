b8441b9aebbd4472ca48cedb067e14251ec8ecda
Mary Khoroshikh <goodmary7@yandex.ru>
2024-11-23 21:17:03 +0300

    1.1.0. images scroll added

--- script.js
+++ script.js
@@ -4,51 +4,54 @@ const imgs = [
         src: "./images/1.jpeg"
     },
     {
+        name: "image description 2",
+        src: "./images/2.jpeg"
+    },
+    {
         name: "image description 3",
         src: "./images/3.jpeg"
     },
     {
+        name: "image description 4",
+        src: "./images/4.jpeg"
+    },
+    {
         name: "image description 5",
         src: "./images/5.jpeg"
-    },
-    {
-        name: "image description 6",
-        src: "./images/6.jpeg"
-    },
-    {
-        name: "image description 7",
-        src: "./images/7.jpeg"
     }
 ];
 
 const imgContainer = document.querySelector('.section-examples__container');
 const image = imgContainer.querySelector('.img');
+const imgDescription = imgContainer.querySelector('.img-description');
 const lengthImg = imgs.length;
 const back = imgContainer.querySelector('.back');
 const forward = imgContainer.querySelector('.forward');
 let i = 0;
 
 function scrollForward() {
-    if (i >= lengthImg) {
+    if (i === lengthImg - 1) {
         i = 0;
+    } else {
+        i += 1;   
     }
     image.src = imgs[i].src;
-    image.name = imgs[i].name;
-    i += 1;
+    imgDescription.textContent = imgs[i].name;
 }
 
 function scrollBack() {
     if (i === 0) {
         i = lengthImg - 1;
+        console.log(i);
+    } else {
+        i -= 1;
     }
     image.src = imgs[i].src;
-    image.name = imgs[i].name;
-    i -= 1;
+    imgDescription.textContent = imgs[i].name;
 }
 
 image.src = imgs[0].src;
-console.log(imgs[0].src);
-console.log(image.src);
+imgDescription.textContent = imgs[0].name;
 
 back.addEventListener('click', () => scrollBack());
 forward.addEventListener('click', () => scrollForward());
